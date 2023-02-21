3615cadb5fd60d98889f7644c95269e1fc8add97
lfreist <freist@informatik.uni-freiburg.de>
2023-02-21 20:50:25 +0100

    fixed all_benchmarks.sh

--- scripts/all_benchmarks.sh
+++ scripts/all_benchmarks.sh
@@ -2,4 +2,4 @@ python3 benchsuit/xs_benchmark.py build-benchmark/xsgrep/xs --iterations 10 --ca
 python3 benchsuit/xs_benchmark.py build-benchmark/xsgrep/xs --iterations 10 -o benchsuit/results/xs/no-cache/$(hostname)
 
 python3 benchsuit/comparison_benchmark.py --iterations 10 --cache -o benchsuit/results/comparison/cache/$(hostname)
-python3 benchsuit/comparison_benchmark.py --iterations 10 --cache -o benchsuit/results/comparison/no-cache/$(hostname)
+python3 benchsuit/comparison_benchmark.py --iterations 10 -o benchsuit/results/comparison/no-cache/$(hostname)
