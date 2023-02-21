cf23b9f98c178d0be5ff76ec6e1fcca25d7032c0
lfreist <freist@informatik.uni-freiburg.de>
2023-02-21 20:48:33 +0100

    benchmarks

---
+++ scripts/all_benchmarks.sh
@@ -0,0 +1,5 @@
+python3 benchsuit/xs_benchmark.py build-benchmark/xsgrep/xs --iterations 10 --cache -o benchsuit/results/xs/cache/$(hostname)
+python3 benchsuit/xs_benchmark.py build-benchmark/xsgrep/xs --iterations 10 -o benchsuit/results/xs/no-cache/$(hostname)
+
+python3 benchsuit/comparison_benchmark.py --iterations 10 --cache -o benchsuit/results/comparison/cache/$(hostname)
+python3 benchsuit/comparison_benchmark.py --iterations 10 --cache -o benchsuit/results/comparison/no-cache/$(hostname)
