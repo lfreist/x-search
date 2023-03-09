a745eb6d53c21777a4b74df6c839630a71d51241
lfreist <freist@informatik.uni-freiburg.de>
2023-03-09 10:49:24 +0100

    example grep

--- example/grep.cpp
+++ example/grep.cpp
@@ -15,17 +15,10 @@ int main(int argc, char** argv) {
   std::string benchmark_format;
 #endif
   std::string pattern;
-  bool line_number = false;
-  bool byte_offset = false;
-  bool ignore_case = false;
-  bool no_mmap = false;
-  bool fixed_strings = false;
-  size_t chunk_size = 16777216;
-  bool count = false;
   std::string file_path;
-  std::string meta_file_path;
+  bool ignore_case = false;
+  bool count = false;
   int num_threads = 0;
-  int num_max_readers = 0;
 
   po::options_description options("Options for example/grep");
   po::positional_options_description positional_options;
@@ -46,29 +39,15 @@ int main(int argc, char** argv) {
       "search pattern");
   add("FILE", po::value<std::string>(&file_path)->default_value(""),
       "input file, stdin if '-' or empty");
-  add("metafile,m", po::value<std::string>(&meta_file_path)->default_value(""),
-      "metafile of the corresponding FILE");
   add("help,h", "prints this help message");
   add("version,V", "display version information and exit");
   add("threads,j",
       po::value<int>(&num_threads)->default_value(0)->implicit_value(0),
       "number of threads");
-  add("max-readers", po::value<int>(&num_max_readers)->default_value(0),
-      "number of concurrent reading tasks (default is number of threads");
   add("count,c", po::bool_switch(&count),
       "print only a count of selected lines");
-  add("byte-offset,b", po::bool_switch(&byte_offset),
-      "print the byte offset with output lines");
-  add("line-number,n", po::bool_switch(&line_number),
-      "print line number with output lines");
   add("ignore-case,i", po::bool_switch(&ignore_case)->default_value(false),
       "ignore case distinctions in patterns and data");
-  add("chunk-size,s", po::value<size_t>(&chunk_size)->default_value(16777216),
-      "min size of a single chunk that is read (default 16 MiB");
-  add("fixed-strings,F", po::bool_switch(&fixed_strings)->default_value(false),
-      "PATTERN is string (force no regex)");
-  add("no-mmap", po::bool_switch(&no_mmap)->default_value(false),
-      "do not use mmap but read data instead");
 #ifdef BENCHMARK
   add("benchmark-file", po::value<std::string>(&benchmark_file),
       "set output file of benchmark measurements.");
@@ -104,21 +83,17 @@ int main(int argc, char** argv) {
   num_threads = num_threads <= 0 ? max_threads : num_threads;
   num_threads = num_threads > max_threads ? max_threads : num_threads;
 
-  // fix number of max readers -------------------------------------------------
-  //  a) <= 0 -> num_threads
-  num_max_readers = num_max_readers <= 0 ? num_threads : num_max_readers;
-
   // ===== Setup xs::Executor for searching ====================================
   if (count) {
     auto searcher = xs::extern_search<xs::count_lines>(
-        pattern, file_path, meta_file_path, ignore_case, num_threads,
-        num_max_readers);
+        pattern, file_path, ignore_case, num_threads);
     searcher->join();
-    std::cout << searcher->getResult()->size();
+    std::cout << searcher->getResult()->size() << std::endl;
   } else {
     auto searcher =
-        xs::extern_search<xs::lines>(pattern, file_path, meta_file_path,
-                                     ignore_case, num_threads, num_max_readers);
+        xs::extern_search<xs::lines>(pattern, file_path,
+                                     ignore_case, num_threads);
+    searcher->join();
     for (auto line : *searcher->getResult()) {
       std::cout << line << '\n';
     }
